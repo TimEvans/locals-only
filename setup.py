8523a166228990fbb1a09f0c2fe0a29b73f24a7c
Tim <teva2622@uni.sydney.edu.au>
2021-07-18 18:28:04 +1000

    Added Makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='locals-only',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/timevans/locals-only',
+    license='',
+    author='Tim Evans',
+    author_email='tim@opacityquantum.com',
+    description='A simple python library for building matrix product operator local Hamiltonians and finding their ground states using DMRG.'
+)
+
